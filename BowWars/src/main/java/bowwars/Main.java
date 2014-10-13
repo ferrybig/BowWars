503c44949c06663a563a5e540fa8d487e3b0b2da
Krijn Toet <krijntoet@online.nl>
2014-10-13 21:49:52 +0200

    Crafting recipes for Grenade launcher, AK 47 and magnum

--- BowWars/src/main/java/bowwars/Main.java
+++ BowWars/src/main/java/bowwars/Main.java
@@ -1,8 +1,18 @@
 package bowwars;
 
-import bowwars.commands.MainCommand; 
-import bowwars.listeners.AreaListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import bowwars.commands.MainCommand;
+import bowwars.listeners.AreaListener;
 
 public class Main extends JavaPlugin {
 
@@ -14,11 +24,45 @@ public class Main extends JavaPlugin {
         this.getCommand("bowwars").setExecutor(mainCommand);
         this.getCommand("bowwars").setTabCompleter(mainCommand);
         this.getServer().getPluginManager().registerEvents(areaListener, this);
+        
+        
+        ShapedRecipe grenadelauncher = new ShapedRecipe(makeItem(Material.BOW, 1, 0, ChatColor.GOLD + "Grenade launcher", "", ChatColor.GRAY + "Will be changed soon!"))
+        .shape("GGG", "GBG", "GRG")
+        .setIngredient('G', Material.SULPHUR)
+        .setIngredient('B', Material.BOW)
+        .setIngredient('R', Material.REDSTONE);
+        Bukkit.addRecipe(grenadelauncher);
+        
+        ShapedRecipe ak = new ShapedRecipe(makeItem(Material.BOW, 1, 0, ChatColor.GOLD + "AK 47", "", ChatColor.GRAY + "Will be changed soon!"))
+        .shape("AAA", "ABA", "ARA")
+        .setIngredient('A', Material.ARROW)
+        .setIngredient('B', Material.BOW)
+        .setIngredient('R', Material.REDSTONE);
+        Bukkit.addRecipe(ak);
+        
+        ShapedRecipe magnum = new ShapedRecipe(makeItem(Material.BOW, 1, 0, ChatColor.GOLD + "Magnum", "", ChatColor.GRAY +  "Will be changed soon!"))
+        .shape("A A", " B ", "ARA")
+        .setIngredient('A', Material.ARROW)
+        .setIngredient('B', Material.BOW)
+        .setIngredient('R', Material.REDSTONE);
+        Bukkit.addRecipe(magnum);
     }
 
     @Override
     public void onDisable() {
         
+    }
+    
+    public ItemStack makeItem(Material m, int amount, int shrt, String name, String lore_1, String lore_2){
+    	ItemStack stack = new ItemStack(m, amount, (short) shrt);
+    	ItemMeta meta = stack.getItemMeta();
+    	meta.setDisplayName(name);
+    	List<String> lore = new ArrayList<String>();
+    	lore.add(lore_1);
+    	lore.add(lore_2);
+    	meta.setLore(lore);
+    	stack.setItemMeta(meta);
+    	return stack;
     }
     
 }
